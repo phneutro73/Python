8f73a110ba8ea6c49de47544eabffa94087baeb6
Roberto Peralta <roberto.peralta@gmail.com>
2021-07-13 21:01:42 +0200

    Ejercicio 6 terminado

--- ejercicio6.py
+++ ejercicio6.py
@@ -1,3 +1,11 @@
 """
-Ejercicio 6. 
+Ejercicio 6. Mostramos todas las tablas de multiplicar del 1 al 10
 """
+
+numero=1
+
+for num in range(1,11):
+    print(f"### TABLA DEL {num} ###")
+    for i in range (1,11):
+        print (f"{i} * {num}\t=",i*num)
+    print("")
